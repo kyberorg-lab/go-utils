6825d72519e490f79e0df08c4263732805e1fd88
Alex Muravya <asm@virtalab.net>
2020-07-16 10:37:18 +0300

    More output

--- main/main.go
+++ main/main.go
@@ -13,17 +13,22 @@ func cryptoTest() {
 	cleanText := "Salasana"
 	sharedSecret := "SharedSecret"
 
+	fmt.Println("Clean Text: ", cleanText, "Shared secret (aka secret key password)", sharedSecret)
+
 	encryptedText, encryptError := aesgcm.EncryptString(cleanText, sharedSecret)
 	if encryptError != nil {
 		fmt.Println(encryptError)
 		panic("Encrypted Error")
 	}
+	fmt.Println("Encrypted text", encryptedText)
 
 	decryptedText, decryptError := aesgcm.DecryptString(encryptedText, sharedSecret)
 	if decryptError != nil {
 		fmt.Println(decryptError)
 		panic("Decrypt Error")
 	}
+	fmt.Println("Decrypted text", decryptedText)
+
 	if decryptedText != cleanText {
 		fmt.Println("Failed. Text mismatch", decryptedText, cleanText)
 	}
